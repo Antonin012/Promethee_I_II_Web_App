10af5e17ffebb94e1426667ee9869b18f0d0a532
Anto <antonin.marouze.etu@univ-lille.fr>
2026-05-24 13:16:49 +0200

    Make : Filled PrometheeCalcul and made each function for make the calcul

---
+++ WEB-INF/classes/controller/PrometheeServlet.java
@@ -0,0 +1,13 @@
+package controller;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.http.HttpClient;
+
+
+@WebServlet("/search")
+public class PrometheeServlet extends HttpServlet{
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        
+	}
+}
--- WEB-INF/classes/model/PrometheeCalcul.java
+++ WEB-INF/classes/model/PrometheeCalcul.java
@@ -1,22 +1,70 @@
 package model;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class PrometheeCalcul {
-    public double calculateNormalization(ArrayList<Alternative> listA,ArrayList<Criterion> listC){
-        return 0.0;
+
+
+    public double calculatePreference(double num, Criterion criterion) {
+        return criterion.getPreference(num);
     }
 
-    public double calculatePreference(double num,Criterion criterion){
-        return 0.0;
+
+    public double[][] computeGlobalPreferenceMatrix(List<Alternative> listA, List<Criterion> listC) {
+        int n = listA.size();
+        double[][] matrix = new double[n][n];
+        
+        double totalWeight = 0;
+        for (Criterion c : listC) {
+            totalWeight += c.getWeight();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    matrix[i][j] = 0.0;
+                }
+                
+                double weightedSum = 0;
+                for (Criterion c : listC) {
+                    double valI = listA.get(i).getValue(c);
+                    double valJ = listA.get(j).getValue(c);
+                    
+                    weightedSum += c.getWeight() * c.getPreference(c.isMaximize() ? (valI - valJ) : (valJ - valI));
+                }
+                
+                matrix[i][j] = (totalWeight > 0) ? (weightedSum / totalWeight) : 0.0;
+            }
+        }
+        return matrix;
     }
 
-    public double[][] computeGlobalPreferenceMatrix(ArrayList<Alternative> listA,ArrayList<Criterion> listC){
-        return new double[0][0];
+
+    public void computeFlows(List<Alternative> alternatives, double[][] matrix) {
+        int n = alternatives.size();
+
+        for (int i = 0; i < n; i++) {
+            double phiPlus = 0;
+            double phiMinus = 0;
+            
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                phiPlus += matrix[i][j];
+                phiMinus += matrix[j][i];
+            }
+            
+            Alternative a = alternatives.get(i);
+            a.setPhiPlus(phiPlus / (n - 1));
+            a.setPhiMinus(phiMinus / (n - 1));
+            a.setPhiNet(a.getPhiPlus() - a.getPhiMinus());
+        }
     }
 
-    public void computeFlows(List<Alternative> alternatives, double[][] matrix){}
-
-    public void computeNetFlows(Alternative alternative){}
+    public void calculate(List<Alternative> alternatives, List<Criterion> criteria) {
+        if (alternatives == null || criteria == null || alternatives.size() < 2) {
+            return;
+        }
+        double[][] matrix = computeGlobalPreferenceMatrix(alternatives, criteria);
+        computeFlows(alternatives, matrix);
+    }
 }
